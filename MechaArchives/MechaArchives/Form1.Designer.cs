01afa9b0e8a6b6387bac80ca19eb19c88755b9b2
Jake K <Jake_Guy_11@protonmail.ch>
2020-08-19 16:56:44 -0400

    Added partial layout

--- MechaArchives/MechaArchives/Form1.Designer.cs
+++ MechaArchives/MechaArchives/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MechaArchives
 {
-    partial class Form1
+    partial class d_formMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(d_formMain));
+            this.d_toolstrip = new System.Windows.Forms.ToolStrip();
+            this.d_uploadButton = new System.Windows.Forms.ToolStripButton();
+            this.d_toolSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.d_pullButton = new System.Windows.Forms.ToolStripButton();
+            this.d_toolSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.d_teamBox = new System.Windows.Forms.GroupBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.d_toolstrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // d_toolstrip
+            // 
+            this.d_toolstrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.d_uploadButton,
+            this.d_toolSeparator1,
+            this.d_pullButton,
+            this.d_toolSeparator2,
+            this.toolStripButton1});
+            this.d_toolstrip.Location = new System.Drawing.Point(0, 0);
+            this.d_toolstrip.Name = "d_toolstrip";
+            this.d_toolstrip.Size = new System.Drawing.Size(670, 25);
+            this.d_toolstrip.TabIndex = 1;
+            this.d_toolstrip.Text = "toolStrip1";
+            // 
+            // d_uploadButton
+            // 
+            this.d_uploadButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.d_uploadButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.d_uploadButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.d_uploadButton.Image = ((System.Drawing.Image)(resources.GetObject("d_uploadButton.Image")));
+            this.d_uploadButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.d_uploadButton.Name = "d_uploadButton";
+            this.d_uploadButton.Size = new System.Drawing.Size(111, 22);
+            this.d_uploadButton.Text = "Upload Robot Data";
+            this.d_uploadButton.ToolTipText = "Upload data about a team\'s robot";
+            // 
+            // d_toolSeparator1
+            // 
+            this.d_toolSeparator1.ForeColor = System.Drawing.SystemColors.Control;
+            this.d_toolSeparator1.Name = "d_toolSeparator1";
+            this.d_toolSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // d_pullButton
+            // 
+            this.d_pullButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.d_pullButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.d_pullButton.Image = ((System.Drawing.Image)(resources.GetObject("d_pullButton.Image")));
+            this.d_pullButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.d_pullButton.Name = "d_pullButton";
+            this.d_pullButton.Size = new System.Drawing.Size(93, 22);
+            this.d_pullButton.Text = "Pull Robot Data";
+            this.d_pullButton.ToolTipText = "Pull data for a specific team\'s robot";
+            this.d_pullButton.Click += new System.EventHandler(this.d_pullButton_Click);
+            // 
+            // d_toolSeparator2
+            // 
+            this.d_toolSeparator2.ForeColor = System.Drawing.SystemColors.Control;
+            this.d_toolSeparator2.Name = "d_toolSeparator2";
+            this.d_toolSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // d_teamBox
+            // 
+            this.d_teamBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.d_teamBox.Location = new System.Drawing.Point(520, 25);
+            this.d_teamBox.Name = "d_teamBox";
+            this.d_teamBox.Size = new System.Drawing.Size(150, 509);
+            this.d_teamBox.TabIndex = 0;
+            this.d_teamBox.TabStop = false;
+            this.d_teamBox.Text = "Available Team Info";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(111, 22);
+            this.toolStripButton1.Text = "Update Robot Data";
+            this.toolStripButton1.ToolTipText = "Pull data for a specific team\'s robot";
+            // 
+            // d_formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(670, 534);
+            this.Controls.Add(this.d_teamBox);
+            this.Controls.Add(this.d_toolstrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "d_formMain";
+            this.Text = " MechaArchives";
+            this.d_toolstrip.ResumeLayout(false);
+            this.d_toolstrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip d_toolstrip;
+        private System.Windows.Forms.ToolStripButton d_uploadButton;
+        private System.Windows.Forms.ToolStripButton d_pullButton;
+        private System.Windows.Forms.ToolStripSeparator d_toolSeparator1;
+        private System.Windows.Forms.GroupBox d_teamBox;
+        private System.Windows.Forms.ToolStripSeparator d_toolSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
     }
 }
 
--- MechaArchives/MechaArchives/Form1.cs
+++ MechaArchives/MechaArchives/Form1.cs
@@ -10,11 +10,33 @@ using System.Windows.Forms;
 
 namespace MechaArchives
 {
-    public partial class Form1 : Form
+    public partial class d_formMain : Form
     {
-        public Form1()
+        public d_formMain()
         {
             InitializeComponent();
+        }
+
+        private void d_pullButton_Click(object sender, EventArgs e)
+        {
+            Button teamButtons; //Raw button that I can edit the settings of later
+            Label myLabel = this.label1;
+            GroupBox teamBox = this.d_teamBox; //The GroupBox that I want the buttons to appear in
+            for(int i = 0; i <16; i++) //8 is just there for now, I'll change it later. It's going downwards so the buttons appear in the proper order (1 -> 8 instead of 8 -> 1)
+            {
+                teamButtons = new Button(); //Initialize the button
+                teamButtons.Name = i.ToString(); //Set the id/name
+                teamButtons.Text = teamButtons.Name; //Set text to the id/name
+                teamButtons.Dock = DockStyle.Top; //Dock to the top
+                teamButtons.Size = new Size(144, 23); //This is the full size of the GroupBox
+                teamButtons.Click += (object sender1, EventArgs e1) => { displayTeamChosen(sender, e, i); };
+                teamBox.Controls.Add(teamButtons); //Add the button
+            }
+        }
+
+        private void displayTeamChosen(object sender, EventArgs e, int n)
+        {
+            d_teamBox.Text = "teamNum is " + n.ToString();
         }
     }
 }
